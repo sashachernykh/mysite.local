30e999ae0c8bf97a1dab7049d6852a467d0bc71d
Sasha Chernykh <lualislu@gmail.com>
2020-06-03 14:58:18 +0300

    3 task

--- index.php
+++ index.php
@@ -2,15 +2,34 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+    <link rel="stylesheet" href="style.css">
+	<title>Mysite Local</title>
 </head>
 <body>
 
-    <h1>Hello, NIX Education</h1>
-	<?php
-      echo "Hello, NIX Education";
-    ?>
+<header class="header" id="header">
+    <div class="container">
+       <nav class="nav">
+           <ul class="menu-list">
+               <li><a href="#">Lesson 1</a></li>
+               <li><a href="#">Lesson 2</a></li>
+           </ul>
+       </nav>
+    </div>
+</header>
 
-	
+<main class="main" id="main">
+    <div class="container">
+        <h1>Hello, <b>NIX Education</b></h1>
+    </div>
+
+</main>
+
+<footer class="footer" id="footer">
+    <div class="container">
+
+    </div>
+
+</footer>
 </body>
 </html>
