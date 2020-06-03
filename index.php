7f3ab6c19ef3658d07f3a70e64cfe32d6897dd1f
Sasha Chernykh <lualislu@gmail.com>
2020-06-03 13:33:25 +0300

    1-2 task

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+    <h1>Hello, NIX Education</h1>
+	<?php
+      echo "Hello, NIX Education";
+    ?>
+
+	
+</body>
+</html>
